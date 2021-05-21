f2938dc596ac169ec952d1441020070ae53adf3b
Gordon Hodanich <ghodanich@gmail.com>
2021-05-21 10:56:54 -0400

    Fixed trenary section

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 function scuberGreetingForFeet(distance){
 if (distance <= 400){
   return "This one is on me!";
-  }
-else if (distance >= 2000 && distance <2499){
+  }else if (distance >= 2000 && distance <2499){
   return "I will gladly take your thirty bucks.";
-  }
-else if (distance > 2500){
+  }else if (distance > 2500){
   return "No can do.";
   }
 }
 
 function ternaryCheckCity(name){
-  let cityName = name === 'NYC' ? 'Ok, sounds good.' : 'No go.';
-return (cityName);
+  return (name === 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
 
 
